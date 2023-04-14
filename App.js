e9aacf164bfa1ddc4314a0afb867ddca01ba1229
6derick6 <6derick6@gmail.com>
2023-04-14 12:57:02 -0300

    App calculadora 1/4

--- App.js
+++ App.js
@@ -1,20 +1,50 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableHighlight, View } from 'react-native';
+import React,{ useState, useEffect } from 'react';
+import Botao from './Botao';
 
 export default function App() {
+
+  console.disableYellowBox = true;
+
+  const [firstNumber, setFirstNumber] = useState(0);
+  const [secondtNumber, setSecondNumber] = useState(0);
+  const [sinal, setSinal] = useState("");
+
+  const [stringCalculo, setStringCalculo] = useState(0);
+
+  var numeros = [];
+
+  for(var i = 0; i <= 9; i++){
+    numeros.push(i);
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{flex:1}}>
+      <View style={styles.topo}><Text style={{fontSize:24}}>{stringCalculo}</Text></View>
+
+      <View style={{flex:1,flexDirection:'row'}}>
+        <TouchableHighlight style={{width:'25%'}}><Text style={{fontSize:24,textAlign:'center'}}>+</Text></TouchableHighlight>
+        <TouchableHighlight style={{width:'25%'}}><Text style={{fontSize:24,textAlign:'center'}}>-</Text></TouchableHighlight>
+        <TouchableHighlight style={{width:'25%'}}><Text style={{fontSize:24,textAlign:'center'}}>/</Text></TouchableHighlight>
+        <TouchableHighlight style={{width:'25%'}}><Text style={{fontSize:24,textAlign:'center'}}>*</Text></TouchableHighlight>
+      </View>
+
+      <View>
+        {
+          numeros.map(function(e){
+            return(<Botao numero={e}></Botao>)
+          })
+        }
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  topo: {
+    padding:10,
+    borderBottomColor:'black',
+    borderBottomWidth:2
+  }
 });
